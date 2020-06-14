371490153cc312dafbdeda13ebddabe966094672
David Sergio <davidsergio52@gmail.com>
2020-06-13 20:43:40 -0700

    added client validation

--- Interview/src/Interview.Web/Controllers/HomeController.cs
+++ Interview/src/Interview.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Interview.Business.Services.StorageServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,5 +16,19 @@ namespace Interview.Web.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<ActionResult> ProcessForm(string emailAddress, [FromForm] IFormFile fileInput)
+        {
+            //IStorageService storageService = new StorageServiceS3("ded20b31-0bf4-4d39-8d1f-9b8aba09cb38");
+            //MemoryStream streamMem = new MemoryStream();
+            //fileInput.CopyTo(streamMem);
+            //streamMem.Seek(0, SeekOrigin.Begin);
+            //(string urlPut, string urlGet, DateTime expiration) accessCredentials = await storageService.UploadStreamLimited(streamMem, fileInput.ContentType, fileInput);
+
+            return Ok("email: " + emailAddress + " file: " + fileInput.FileName);
+        }
+
     }
 }
